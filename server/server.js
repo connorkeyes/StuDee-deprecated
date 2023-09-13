1beb273065fc5a157fc3d42d295adcb753a34069
Connor Keyes <connorkeyes@Connors-MacBook-Air.local>
2023-09-13 11:29:52 -0700

    express server setup and mongodb schema & models

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const mongoose = require('mongoose');
+
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+app.get('/styles.css', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, '../client/styles.css'));
+});
+
+app.get('/client/index.js', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, '../client/index.js'));
+});
+
+
+
+// Unknown route handler
+app.use((req, res) => res.sendStatus(404));
+
+// Global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
---
+++ server/userModels.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const MONGO_URI = 'mongodb+srv://connorkeyes:D36U8CGSL5maEh9h@studee.tjkzjjf.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to Mongo DB.'))
+  .catch(err => console.log(err));
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+  dataSets: {type: [dataSetSchema], default: []}
+});
+
+const dataSetSchema = new Schema({
+  flashCards: {type: [flashcardSchema], default: []}
+});
+
+const flashcardSchema = new Schema({
+  front: {type: String, required: true},
+  back: {type: String, required: true}
+});
+
+
+
+module.exports = {
+
+};
